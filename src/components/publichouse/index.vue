078f5ae651f75d67488806ddf646dfb50cdc5703
hechengxin <sq888555@163.com>
2021-11-12 14:38:23 +0800

    代码提交

--- src/components/publichouse/index.vue
+++ src/components/publichouse/index.vue
@@ -62,7 +62,7 @@
           </div>
         </sideItem>
         <sideItem title="土地情况" transitionType="right" delay="200" height="37.67%">
-          <div slot="body" class="landState">
+          <div slot="body" class="landState" style="height: 100%;width: 100%;">
             <div class="chart-land" id="landChart"></div>
           </div>
         </sideItem>
@@ -98,6 +98,7 @@ import nowpeopleslide from '../nowpeopleslide.vue'
 import sideItem from '../sideItem.vue'
 import currency from '../currency.vue'//通用box组件
 import AnimatedNumber from "animated-number-vue";
+import 'echarts-gl'
 import {
   mapGetters
 } from 'vuex'
@@ -139,6 +140,8 @@ export default {
         }, 500);
       })
     },
+		
+		
     //学校概况
     getSchoolState() {
       this.staList = [
@@ -389,6 +392,9 @@ export default {
       };
       chart.clear();//清除动画
       chart.setOption(option, true);
+			window.addEventListener("resize", function() {
+			  chart.resize()
+			})
     },
 
     initcolor() {
@@ -563,172 +569,7 @@ export default {
 					}, ]
 				});
 			}
-    // rendpubpie() { //能耗态势设备能耗占比饼状图,右侧的那个数据不能有中echarts中的legend需要自己写,点击右边数据不和echarts做联动
-    // 	console.log("进入")
-    // 	let usepublicChartDom, usepublicChartChart, option
-    // 	usepublicChartDom = document.getElementById('usepublic');
-    // 	usepublicChartChart = echarts.init(usepublicChartDom, {
-    // 		width: 150,
-    // 		height: 150
-    // 	});
-    // 	let names = ["生活用水", "浇灌用水", "其他"];
-    // 	let data1 = [1114, 444, 501]
-    // 	let list = []
-    // 	let total = 0
-    // 	for (let i in data1) {
-    // 		total += data1[i]
-    // 	}
-
-    // 	let placeHolderStyle = {
-    // 		normal: {
-    // 			label: {
-    // 				show: false
-    // 			},
-    // 			labelLine: {
-    // 				show: false
-    // 			},
-    // 			color: 'rgba(0, 0, 0, 0)',
-    // 			borderColor: 'rgba(0, 0, 0, 0)',
-    // 			borderWidth: 0
-    // 		}
-    // 	};
-
-    // 	let rich = {
-    // 		white: {
-    // 			align: 'center',
-    // 			padding: [3, 0]
-    // 		}
-    // 	};
-
-    // 	for (let i in data1) {
-    // 		list.push({
-    // 			value: data1[i],
-    // 			name: names[i],
-    // 			itemStyle: {
-    // 				normal: {
-    // 					borderWidth: 5,
-    // 					shadowBlur: 20,
-    // 					borderColor: color[i],
-    // 					shadowColor: color[i],
-    // 					color: color[i]
-    // 				}
-    // 			}
-    // 		}, {
-    // 			value: total / 30,
-    // 			name: '',
-    // 			itemStyle: placeHolderStyle
-    // 		})
-    // 	}
-
-    // 	let func = (params) => {
-    // 		let percent = ((params.value / total) * 100).toFixed(1)
-    // 		let name = params.name.replace(/\n/g, '')
-    // 		if (params.name !== '') {
-    // 			return name + '\n{white|' + percent + '%}'
-    // 		} else {
-    // 			return ''
-    // 		}
-    // 	}
-
-
-
-    // 	usepublicChartChart.setOption({
-    // 		tooltip: {
-    // 			show: false
-    // 		},
-    // 		grid: {
-    // 			containLabel: true,
-    // 			left: 30,
-    // 			top: 0,
-    // 			bottom: 0
-    // 		},
-    // 		legend: {
-    // 			show: false,
-    // 			orient: 'vertical',
-    // 			data: names,
-    // 			icon: 'circle',
-    // 			right: '5px',
-    // 			top: '10px',
-    // 			textStyle: {
-    // 				color: '#fff',
-    // 				fontSize: 20
-    // 			}
-    // 		},
-    // 		series: [{
-    // 				name: '',
-    // 				type: 'pie',
-    // 				clockWise: false,
-    // 				startAngle: '90',
-    // 				center: ['50%', '50%'],
-    // 				radius: ['70%', '70%'], //设置饼状图的宽高
-    // 				hoverAnimation: false,
-    // 				itemStyle: {
-    // 					normal: {
-    // 						label: {
-    // 							show: false,
-    // 							position: 'outside',
-    // 							formatter: func,
-    // 							rich: rich
-    // 						},
-    // 						labelLine: {
-    // 							length: 40,
-    // 							length2: 100,
-    // 							show: false,
-    // 							color: '#00ffff'
-    // 						}
-    // 					}
-    // 				},
-    // 				data: list,
-    // 				animationType: 'scale',
-    // 				animationEasing: 'elasticOut',
-    // 				animationDelay: function(idx) {
-    // 					return idx * 350;
-    // 				}
-    // 			},
-    // 			{
-    // 				name: '',
-    // 				type: 'pie',
-    // 				center: ['50%', '50%'],
-    // 				radius: ['49%', '49%'],
-    // 				itemStyle: {
-    // 					color: 'transparant'
-    // 				},
-    // 				startAngle: '90',
-    // 				data: [{
-    // 					value: total,
-    // 					name: '',
-    // 					label: {
-    // 						normal: {
-    // 							show: true,
-    // 							formatter: '{c|设备}' + '\n\r' + '{active|用电占比}',
-
-    // 							// formatter: '用电纪录',
-    // 							rich: {
-    // 								c: {
-    // 									color: 'rgba(255, 255, 255, .5)',
-    // 									fontSize: 12,
-    // 									// fontWeight: 'bold',
-    // 									lineHeight: 22
-    // 								},
-    // 								b: {
-    // 									color: 'rgba(255, 255, 255, .5)',
-    // 									fontSize: 12,
-    // 									lineHeight: 22
-    // 								}
-    // 							},
-    // 							textStyle: {
-    // 								fontSize: 12,
-    // 								// fontWeight: 'bold'
-    // 							},
-    // 							position: 'center'
-    // 						}
-    // 					}
-    // 				}]
-    // 			}
-    // 		]
-    // 	});
-    // 	// usepublicChartChart.setOption(option)
-    // }
+    
   }
 }
 </script>
@@ -833,15 +674,18 @@ export default {
       }
     }
   }
-  .landState {
-    padding: 0 16px;
-    width: 100%;
+  // .landState {
+  //   padding: 0 16px;
+  //   width: 100%;
+  //   .chart-land {
+  //     width: 100%;
+  //     height: 80%;
+  //   }
+  // }
     .chart-land {
-      width: 340px;
-      height: 280px;
+      width: 100%;
+      height: 100%;
     }
-  }
-
   #allTotalAssets {
     width: 100%;
     height: 150px;
