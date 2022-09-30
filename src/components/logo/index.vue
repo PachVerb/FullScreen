0566ccdd8c28223ed05ed7f20bb26cb87a848e55
wangshan <wangshan@nbtide.net>
2022-09-30 22:08:34 +0800

    feat(LOGO): 标题替换

--- src/components/logo/index.vue
+++ src/components/logo/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="logo-wrap">
-    <img class="logo" alt="logo" src="../../assets/img/toptitle.png" />
+    <img class="logo" alt="logo" src="../../assets/img/toptitle3.png" />
     <svg class="svg-box">
       <polyline class="ani-line1" points="30,20 200,20" />
       <polyline class="ani-line2" points="1890,20 1720,20" />
@@ -9,34 +9,32 @@
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style lang="less" scoped>
-.logo-wrap{
+.logo-wrap {
   position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
 }
-.logo{
+.logo {
   display: inline-block;
-  width: 95%; 
+  width: 95%;
 }
-.logo2-wrap{
+.logo2-wrap {
   position: absolute;
   top: 7px;
   width: 107%;
   height: 119%;
 }
-.logo2{
+.logo2 {
   width: 100%;
   height: 100%;
 }
-.logo2-box{
+.logo2-box {
   position: absolute;
   top: 0;
   left: 50%;
