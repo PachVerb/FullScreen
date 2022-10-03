9c83061083e0485b90af28b16465ee96876da64f
wangshan <wangshan@nbtide.net>
2022-10-03 22:07:22 +0800

    feat: 登陆屏蔽

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,10 @@
+/*
+ * @LastEditTime: 2022-10-03 22:05:40
+ * @Description:
+ * @Date: 2022-10-03 14:56:03
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Index from "../views/index.vue";
@@ -5,27 +12,28 @@ import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
-const routes = [{
-        path: "/",
-        name: "Index",
-        component: Index,
-    }, {
-        path: "/Login",
-        name: "Login",
-        component: Login
-    },
-
+const routes = [
+  {
+    path: "/",
+    name: "Index",
+    component: Index,
+  },
+  //   {
+  //     path: "/Login",
+  //     name: "Login",
+  //     component: Login,
+  //   },
 ];
 
 const router = new VueRouter({
-    routes
+  routes,
 });
-router.beforeEach(function(to, from, next) {
-    if (!localStorage.getItem("username")) {
-        if (to.path !== '/Login') {
-            return next('/Login')
-        }
-    }
-    next()
-})
+// router.beforeEach(function(to, from, next) {
+//     if (!localStorage.getItem("username")) {
+//         if (to.path !== '/Login') {
+//             return next('/Login')
+//         }
+//     }
+//     next()
+// })
 export default router;
